88638523edfee80d6cc8b6eb8a30817a1144c369
Cássio Quintana <cassioquint@gmail.com>
2021-07-08 19:36:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { request } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({message: 'Server is up!'});
+});
+
+app.listen(3333);
